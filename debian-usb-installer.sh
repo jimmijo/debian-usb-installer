e57f551acad58105c45202116d3b0cf5ec93c2f4
jimmijo <jimmijo.dev@gmail.com>
2016-02-20 00:19:29 +0100

    Create the structure of the app.

---
+++ debian-usb-installer.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+function usage {
+    # TODO
+}
+
+function init {
+    # TODO
+}
+
+function parseCli {
+    # TODO
+}
+
+function checkUsb {
+    # TODO
+}
+
+function downloadDebian {
+    # TODO
+}
+
+function copyDebianToUsb {
+    # TODO
+}
